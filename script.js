8cca3a5eebe9dd050ab152b6a6218efd25a35baa
Matheus <matheusdomingues10@gmail.com>
2022-06-25 20:11:17 -0300

    Todos os Requisitos obrigatórios completos

--- script.js
+++ script.js
@@ -1,66 +1,121 @@
-const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-promise.then(retorno);
+// Envio de Nome
 
-function retorno(mensagem){
-    console.log(mensagem)
+let nomeUsuario;
+let nomeRecebido;
+
+criarNome();
+
+function criarNome(){
+    nomeUsuario = prompt("Insira aqui um nome/apelido válido!");
+    nomeValidacao();  
+}
+
+function nomeValidacao(){
+    nomeRecebido = {
+        name: `${nomeUsuario}`
+    }
+}
+
+let exportaNome = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nomeRecebido);
+exportaNome.then(nomeValido)
+exportaNome.catch(nomeInvalido)
+
+function nomeValido(){    
+    renderizarMensagem();
+}
+
+function nomeInvalido(){    
+    alert("Nome/Apelido já existente! Escolha outro!");
+    criarNome();
+}
+
+// Buscar Mensagens // 
+
+refrescaChat();
+
+function refrescaChat(){
+    setInterval(usuarioAtivo, 5000);
+}
+
+function usuarioAtivo(){
+    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nomeRecebido);
+    promise.then(atualizou);
+}
+
+function atualizou(){
+    return;
+}
+
+function renderizarMensagem(){
+
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(retorno);
+}
+
+function retorno(mensagem){    
     const pacoteMensagens = document.querySelector(".meio");
 
-    for(let i = 0; i < 10; i++){
-     if(mensagem.data[i].type === "status"){
-    pacoteMensagens.innerHTML = `<div
-        class="msgs status"> 
-        <h1>(${mensagem.data[i].time})</h1>
-        <h2>${mensagem.data[i].from}</h2>
-        <h3>${mensagem.data[i].text}</h3>
-        </div>`;
+    for(let i = 0; i < mensagem.data.length; i++){
+        
+    if(mensagem.data[i].type === "status"){
+    pacoteMensagens.innerHTML += `<div
+    class="msgs status"> 
+    <h1>(${mensagem.data[i].time})</h1>
+    <h2>${mensagem.data[i].from}</h2>
+    <h3>${mensagem.data[i].text}</h3>
+    </div>`;}
+
+    if (mensagem.data[i].type === "message"){
+    pacoteMensagens.innerHTML += `<div
+    class="msgs message"> 
+    <h1>(${mensagem.data[i].time})</h1>
+    <h2>${mensagem.data[i].from}</h2> para <h2>${mensagem.data.to}:</h2>
+    <h3>${mensagem.data[i].text}</h3>
+    </div>`;}
+
+    if (mensagem.data[i].type === "private_message" && mensagem.data.to){
+    pacoteMensagens.innerHTML += `<div
+    class="msgs private"> 
+    <h1>(${mensagem.data[i].time})</h1>
+    <h2>${mensagem.data[i].from}</h2> reservadamente para <h2>${mensagem.data.to}:</h2>
+    <h3>${mensagem.data[i].text}</h3>    
+    </div>`;}    
     }
-    if (mensagem.data[i].type === "message") {
-        pacoteMensagens.innerHTML = `<div
-        class="msgs message"> 
-        <h1>${mensagem.data[i].time}</h1>
-        <h2>${mensagem.data[i].from}</h2> para <h2>${mensagem.data.to}</h2>: 
-        <h3>${mensagem.data[i].text}</h3>
-        </div>`; 
+
+    const rolagemAuto = document.querySelector('.meio div:last-child');
+    rolagemAuto.scrollIntoView();
+}
+
+// Enviar Mensagem //
+
+function enviarMensagem(){
+
+    let mensagemEnviar = document.querySelector(".mensagemfeita").value;
+    
+    let mensagemCompleta = {
+        from: `${nomeUsuario}`, to: "todos", text: `${mensagemEnviar}`, type: "message",
     }
-    if (mensagem.data[i].type === "private_message") {
-        pacoteMensagens.innerHTML = `<div
-        class="msgs private"> 
-        <h1>${mensagem.data[i].time}</h1>
-        <h2>${mensagem.data[i].from}</h2> reservadamente para <h2>${mensagem.data.to}</h2>: 
-        <h3>${mensagem.data[i].text}</h3>    
-        </div>`;
-    }    
-}
-}
 
-/* axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nomeUsuario);
+    console.log(mensagemCompleta)
+    
+    let requerirMensagem = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagemCompleta);
+    requerirMensagem.then(envioValido);
+    requerirMensagem.catch(envioInvalido);
+    
+    apagarInput();
+} 
 
-function novoNome(){
-const nomeUsuario = {
-    nome: prompt("Insira aqui um nome/apelido válido!")
-}
+function apagarInput(){
+    let apagaInput = document.querySelector(".base");
+    apagaInput.innerHTML = `<img onclick="enviarMensagem()" src="img/Vector2.png" />`
+    apagaInput.innerHTML = `<input type="text" class="mensagemfeita" placeholder="Escreva aqui..."/>
+  <img onclick="enviarMensagem()" src="img/Vector2.png" />`
 }
 
-Promise.then(nomeValido)
-Promise.catch(nomeInvalido)
-
-function nomeValido(){
-     // fazer algum comando que direcione ao bate-papo já integrado com o nome escolhido
+function envioValido(){
+    renderizarMensagem();
 }
 
-function nomeInvalido(erro){
-    if(erro.reponse.status === 400){
-        alert("Nome/Apelido já existente! Escolha outro!");
-        novoNome();
-    }
-}
-
-axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nomeValido);
-
-function enviarMensagem(textoEnviar){
-    const mensagemCompleta = `from: ${nomeValido}, to: ${todos/especifico}, text: ${textoEnviar}, type: ${private/message}`;
-    axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagemCompleta);
-}
-
-
-*/ 
+function envioInvalido(erro){
+   console.log(erro.data.status)
+} 
